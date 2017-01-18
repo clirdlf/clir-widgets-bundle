f8ec37acde50c19bd48f12af41d0c1db0aa4128c
Wayne Graham <wgraham@clir.org>
2017-01-18 11:30:44 -0500

    Add Community Calendar Shortcode
    Adds a shortcode for embedding the DLF Community Calendar in WordPress

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -20,13 +20,42 @@ function clir_clearfix()
 }
 
 /**
+ * For embedding the DLF Community Calendar (managed in Google Calendar)
+ *
+ * @return String embed code for the community calendar. Can be overridden for
+ * other Google calendars
+ */
+function community_calendar( $atts )
+{
+  $a = shortcode_atts(array(
+      'width'       => '800',
+      'height'      => '600',
+      'src'         => 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
+      'bgcolor'     => '#FFFFFF',
+      'color'       => '#2F6309',
+      'ctz'         => 'America/New_York',
+      'frameborder' => 0,
+      'scrolling'   => 'no',
+      'style'       => 'border-width:0'
+  ), $atts);
+
+  $url = 'https://calendar.google.com/calendar/embed?height=' . $a['height'];
+  $url .= '&amp;wkst=1&amp;bgcolor=' . urlencode($a['bgcolor']);
+  $url .= '&amp;src=' . urlencode($a['src']);
+  $url .= '&amp;color=' . urlencode($a['color']) . '&amp;ctz=' . urlencode($a['ctz']);
+
+  $iframe = "<iframe src='{$url}' style='{$a['style']}' width='{$a['width']}' height='{$a['height']}' frameborder='{$a['frameborder']}' scrolling='{$a['scrolling']}'></iframe>";
+  return $iframe;
+}
+
+/**
  * Used in DLF theme...from old Construct theme (which is why it's gross)
  *
  * @return String
  */
-function image_frame( $atts = null, $content = null ) {
+/** function image_frame( $atts = null, $content = null ) {
   if( $atts == 'generator' ) {
-    $option = array( 
+    $option = array(
       'name' => __( 'Image Frames', 'clir' ),
       'value' => 'image_frame',
       'options' => array(
@@ -141,12 +170,13 @@ function image_frame( $atts = null, $content = null ) {
 
   return $out;
 }
-
+*/
 
 function register_shortcodes()
 {
   add_shortcode('clearboth', 'clir_clearfix');
-  add_shortcode('image_frame', 'clir_clearfix');
+  add_shortcode('community_calendar', 'community_calendar');
+  // add_shortcode('image_frame', 'clir_clearfix');
 }
 
 add_action('init', 'register_shortcodes');
