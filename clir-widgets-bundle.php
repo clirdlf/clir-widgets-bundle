3a2bca61980c7d9a5fe0be7f999a941098113972
Wayne Graham <wgraham@clir.org>
2017-01-31 12:52:34 -0500

    Bump revision number

--- clir-widgets-bundle.php
+++ clir-widgets-bundle.php
@@ -6,14 +6,14 @@
  * Text Domain: clir-widgets-bundle
  * Domain Path: /languages
  * Author: Council on Libraries and Information Resources
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author URI: https://www.clir.org
  * License: GPL3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
  */
 
 define('CLIR_WIDGETS_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('CLIR_WIDGETS_PLUGIN_URL',  plugin_dir_url(__FILE__));
+define('CLIR_WIDGETS_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
 require_once CLIR_WIDGETS_PLUGIN_PATH . 'lib/calendar-widget.php';
 require_once CLIR_WIDGETS_PLUGIN_PATH . 'lib/social-media-links.php';
