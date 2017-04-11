e46f502808e86e51edebcb97bbb392cc9369ecd6
Wayne Graham <wgraham@clir.org>
2017-04-11 15:22:38 -0400

    Add shortcode output for the dlf intro bits

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -167,8 +167,106 @@ function clir_reports_view( $attr )
 
     $iframe = "<iframe src='{$url}' style='{$a['style']}' width='{$a['width']}' height='{$a['height']}' frameborder='{$a['frameborder']}' scrolling='{$a['scrolling']}'></iframe>";
     $iframe .= "<p style='font-size: smaller;'>Maintained by the <a href='https://www.diglib.org/opportunities/calendar/'>Digital Library Federation</a></p>";
-      return $iframe;
+
+    return $iframe;
+}
+
+function random_image( $dir )
+{
+
+}
+
+function dlf_post( $atts )
+{
+  //TODO: get an array of random images to get thumbs for
+  $a = shortcode_atts(array(
+    'category' => 'blog',
+  ), $atts);
+
+  $category_id = get_cat_id($a['category']);
+  $category_link = get_category_link($category_id);
+
+  $args = array(
+    'numberposts' => 1,
+    'category'    => $category_id,
+    'post_type'   => 'post',
+    'post_status' => 'publish',
+    'order'       => 'DESC',
+    'orderby'     => 'post_date',
+  );
+
+  $posts = wp_get_recent_posts( $args );
+  $post_id = $posts[0]['ID'];
+  $post = $posts[0];
+
+  // $post = get_post($post_id);
+
+  $thumb = 'https://www.placecage.com/270/270';
+  if ( has_post_thumbnail() ) {
+  	$thumb = the_post_thumbnail(array(270,270)); // @see https://developer.wordpress.org/reference/functions/the_post_thumbnail/
   }
+  setup_postdata($post);
+
+
+  $output = '<div class="col-md-6">';
+  $output .= '<article id="post-' . $post_id . '" class="post" itemtype="http://schema.org/BlogPosting">';
+    $output .= '<div class="rowtight">';
+      $output .= '<div class="imagehoverclass" itemprop="image" itemtype="https://schema.org/ImageObject">';
+        $output .= '<a href="'. get_permalink($post["ID"]) . '"><img alt="'. esc_attr($post['post_title']) .'" src="'. $thumb .'" /></a>';
+      $output .= '</div>';
+      $output .= '<div class="postcontent">';
+        $output .= '<div class="postmeta updated color_gray">';
+          $output .= '<div class="postdate bg-lightgray headerfont" itemprop="datePublished">';
+            $output .= '<span class="postday">' . get_the_date('d', $post_id) . '</span> ' . get_the_date('M Y', $post_id);
+          $output .= '</div>';
+          $output .= '<header class="home_blog_title">';
+            $output .= '<a href="'. get_permalink($post["ID"]) . '">';
+              $output .= '<h4 class="entry-title" itemprop="name headline">' . get_the_title($post['ID']) . '</h4>';
+            $output .= '</a>';
+            $output .= '<div class="subhead color_gray">';
+              $output .= '<span class="category"><i class="fa fa-folder-open"></i>';
+                $output .= '<a href="'.esc_url( $category_link ) .'" title="'. $a['category'] .'">' . $a['category'] . '</a>';
+              $output .= '</span>';
+            $output .= '</div>';
+          $output .= '</header>';
+          $output .= '<div class="entry-content">';
+            $output .= wp_trim_words($post['post_content']);
+          $output .= '</div>';
+          $output .= '<footer>';
+            $output .= '<meta itemscope="" itemprop="mainEntityOfPage" itemtype="https://schema.org/WebPage" itemid="'. $post['guid'] . '">';
+            $output .= '<meta itemprop="dateModified" content="'. $post['post_modified_gmt'] .'" />';
+            $output .= '<div itemprop="publisher" itemtype="https://schema.org/Organization">';
+              $output .= '<div itemprop="logo" itemscope="" itemtype="https://schema.org/ImageObject">';
+                $output .= '<meta itemprop="url" content="https://www.diglib.org/wp-content/uploads/2013/07/DLFrev1BL_notag_200.png">';
+                $output .= '<meta itemprop="width" content="200">';
+                $output .= '<meta itemprop="height" content="84">';
+              $output .= '</div>';
+              $output .= '<meta itemprop="name" content="'. $post['post_title'] .'">';
+            $output .= '</div>';
+          $output .= '</footer>';
+        $output .= '</div>';
+      $output .= '</div>';
+    $output .= '</div>';
+  $output .= '</article>';
+
+
+
+  $output .= "</div>";
+
+  // $output .= debug($post);
+
+  // wp_reset_postdata();
+  return $output;
+}
+
+function debug($content)
+{
+  $output = "<pre>";
+  $output .= var_dump($content);
+  $output .= "</pre>";
+
+  return $output;
+}
 
 function register_shortcodes()
 {
@@ -176,6 +274,43 @@ function register_shortcodes()
   add_shortcode('community_calendar', 'community_calendar');
   add_shortcode('recent_publications', 'clir_reports_view');
   add_shortcode('email', 'hide_email');
+  add_shortcode('dlf_post', 'dlf_post');
 }
 
 add_action('init', 'register_shortcodes');
+
+// Utility Functions TODO refactor to utilities.php
+/**
+ * Create an excerpt of an arbitrary length for a given Post
+ *
+ * @see https://codex.wordpress.org/Function_Reference/get_the_excerpt
+ *
+ * @param Post $post Post to generate excerpt from
+ * @param int  $length Length of the excerpt
+ *
+ * @return String Excerpt of length $length
+ */
+ function the_excerpt_max_charlength($post, $charlength)
+ {
+     $excerpts = get_the_excerpt( get_post($post['ID']));
+     $excerpt = $excerpts[0];
+
+     $output = "";
+     $charlength++;
+
+     return $excerpts;
+
+     if (mb_strlen($excerpt) > $charlength) {
+         $subex = mb_substr($excerpt, 0, $charlength - 5);
+         $exwords = explode(' ', $subex);
+         $excut = - (mb_strlen($exwords[ count($exwords) - 1 ]));
+         if ($excut < 0) {
+             $output = mb_substr($subex, 0, $excut);
+         } else {
+             $output = $subex;
+         }
+         echo '[...]';
+     }
+
+     return $output;
+ }
