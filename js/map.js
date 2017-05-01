c2334329010ea10a56e304ef57526dd2d7546e18
Wayne Graham <wgraham@clir.org>
2017-05-01 13:06:25 -0400

    Code cleanup

--- js/map.js
+++ js/map.js
@@ -36,7 +36,6 @@
   });
 
   // console.log(php_vars.layer);
-
   var geoJsonLayer = L.geoJson(dlf, { // TODO: pass layer properly @see https://github.com/waynegraham/clir-widgets-bundle/issues/3
     onEachFeature: onEachFeature,
     pointToLayer: function(feature, latlng) {
